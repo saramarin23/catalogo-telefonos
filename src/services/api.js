adf9e853f238cb3ef37f013337543ca619c4f56c
saramarin23 <saramarin.econ@gmail.com>
2025-02-08 01:04:53 +0100

    fetch product by id

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 const API_URL = import.meta.env.VITE_API_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
+const url = new URL(`${API_URL}/products`);
+
 export const fetchAllProducts = async (searchQuery = '') => {
   try {
-    const url = new URL(`${API_URL}/products`);
 
     if (searchQuery.trim() !== '') {
       url.searchParams.append('search', searchQuery);
@@ -23,6 +24,27 @@ export const fetchAllProducts = async (searchQuery = '') => {
     return data;
   } catch (error) {
     console.error('Error al obtener los productos', error);
+    throw error;
+  }
+}
+
+export const fetchProductById = async(id) => {
+  try {
+    const response = await fetch(`${url}/${id}`, {
+      method: 'GET',
+      headers: {
+        'x-api-key': API_KEY,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error al obtener el producto', error);
     throw error;
   }
 }
