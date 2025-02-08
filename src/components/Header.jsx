12594269db8f07b6dc7950276d443b2befe19f4f
saramarin23 <saramarin.econ@gmail.com>
2025-02-08 19:09:16 +0100

    hide cart icon in cart page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Logo from '../assets/logo.png';
 import CartIconBlack from '../assets/icons/CartIconBlack.svg';
 import CartIconWhite from '../assets/icons/CartIconWhite.svg';
@@ -6,6 +6,7 @@ import { useCart } from '../context/CartContext';
 
 const Header = () => {
   const { cartItems } = useCart();
+  const location = useLocation();
 
   return (
     <header className="header">
@@ -13,22 +14,24 @@ const Header = () => {
       <Link to="/">
         <img src={Logo} className="logo" />
       </Link>
-      {/* TODO:  empty ? white : black */}
-      <div
-        style={{
-          display: 'flex',
-          cursor: 'pointer',
-          padding: '4px 0',
-          alignItems: 'center',
-          gap: '6px',
-        }}
-      >
-        <img
-          src={cartItems.length > 0 ? CartIconBlack : CartIconWhite}
-          style={{ width: '16px', height: '16px' }}
-        />
-        <p>{cartItems.length}</p>
-      </div>
+      {location.pathname !== '/cart' && (
+        <Link
+          to="/cart"
+          style={{
+            display: 'flex',
+            cursor: 'pointer',
+            padding: '4px 0',
+            alignItems: 'center',
+            gap: '6px',
+          }}
+        >
+          <img
+            src={cartItems.length > 0 ? CartIconBlack : CartIconWhite}
+            style={{ width: '16px', height: '16px' }}
+          />
+          <p>{cartItems.length}</p>
+        </Link>
+      )}
     </header>
   );
 };
