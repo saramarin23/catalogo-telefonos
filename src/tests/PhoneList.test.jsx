ffafaecb32d2d3c3c1ba3e9bc30e158471104c9b
saramarin23 <saramarin.econ@gmail.com>
2025-02-09 10:48:58 +0100

    refactor PhoneList test file and ensure correct filtering

--- src/tests/PhoneList.test.jsx
+++ src/tests/PhoneList.test.jsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { vi, describe, it, beforeEach, expect } from 'vitest';
+import { vi, describe, it, expect } from 'vitest';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import PhoneList from '../views/PhoneListView';
 import { fetchAllProducts } from '../services/api';
@@ -9,39 +9,36 @@ vi.mock('../services/api', async () => ({
   fetchAllProducts: vi.fn(),
 }));
 
-describe('PhoneList', () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
+const mockProducts = [
+  { id: 1, name: 'Phone 1' },
+  { id: 2, name: 'Phone 2' },
+  { id: 3, name: 'Phone 3' },
+];
 
-    fetchAllProducts.mockImplementation(async () => [
-      { id: 1, name: 'Phone 1' },
-      { id: 2, name: 'Phone 2' },
-      { id: 3, name: 'Phone 3' },
-    ]);
+const renderPhoneListComponent = async () => {
+  vi.clearAllMocks();
+  fetchAllProducts.mockResolvedValue(mockProducts);
+
+  render(
+    <MemoryRouter>
+      <PhoneList />
+    </MemoryRouter>,
+  );
+
+  await waitFor(() => {
+    expect(screen.getByText('Phone 1')).toBeInTheDocument();
   });
+};
 
+describe('PhoneList', () => {
   it('renders products after fetching data', async () => {
-    render(
-      <MemoryRouter>
-        <PhoneList />
-      </MemoryRouter>,
-    );
-
-    await waitFor(() => {
-      expect(screen.getByText('Phone 1')).toBeInTheDocument();
-    });
+    await renderPhoneListComponent();
+    expect(screen.getByText('Phone 2')).toBeInTheDocument();
+    expect(screen.getByText('Phone 3')).toBeInTheDocument();
   });
 
   it('filters products when the user writes in the searchbar', async () => {
-    render(
-      <MemoryRouter>
-        <PhoneList />
-      </MemoryRouter>,
-    );
-
-    await waitFor(() => {
-      expect(screen.getByText('Phone 1')).toBeInTheDocument();
-    });
+    await renderPhoneListComponent();
 
     const searchInput = screen.getByPlaceholderText(
       'Search for a smartphone...',
@@ -51,6 +48,13 @@ describe('PhoneList', () => {
 
     await waitFor(() => {
       expect(screen.getByText('Phone 1')).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      expect(screen.queryByText('Phone 2')).not.toBeInTheDocument(),
+        { timeout: 2000 };
+      expect(screen.queryByText('Phone 3')).not.toBeInTheDocument(),
+        { timeout: 2000 };
     });
   });
 });
