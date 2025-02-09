0b7f482e0056487194bda66ee09c5ed5aeaef495
saramarin23 <saramarin.econ@gmail.com>
2025-02-09 12:56:15 +0100

    force PhoneDetail page to re-render after clicking on similar products card

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
-import PhoneList from './views/PhoneList';
-import PhoneDetail from './views/PhoneDetail';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import PhoneList from './views/PhoneListView';
+import PhoneDetail from './views/PhoneDetailView';
 import CartView from './views/CartView';
 import Header from './components/Header';
 import './styles/main.scss';
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<PhoneList />} />
-        <Route path="/products/:id" element={<PhoneDetail />} />
+        <Route
+          path="/products/:id"
+          element={<PhoneDetail key={location.pathname} />}
+        />
         <Route path="/cart" element={<CartView />} />
       </Routes>
     </>
