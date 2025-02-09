2cd1babf3d1b6478af01b6477fcaf7f017cc0bf3
saramarin23 <saramarin.econ@gmail.com>
2025-02-09 12:59:28 +0100

    separate routing from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
-import PhoneList from './views/PhoneListView';
-import PhoneDetail from './views/PhoneDetailView';
-import CartView from './views/CartView';
-import Header from './components/Header';
-import './styles/main.scss';
+import AppRoutes from './AppRoutes';
 
 function App() {
-  const location = useLocation();
-
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<PhoneList />} />
-        <Route
-          path="/products/:id"
-          element={<PhoneDetail key={location.pathname} />}
-        />
-        <Route path="/cart" element={<CartView />} />
-      </Routes>
-    </>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
